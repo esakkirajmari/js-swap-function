798e1b8960e3b36602984726924ed873498498c8
esakkirajmari <esakkirajmari@gmail.com>
2022-12-15 16:03:02 +0530

    index.html

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var a,b,c;
+a=(prompt("enter the value"));
+b=(prompt("enter the value"));
+    function swap(){
+        c=a;
+        a=b;
+        b=c;
+
+        document.write(a+"<br>")
+        document.write(b)
+    }
+    swap()
